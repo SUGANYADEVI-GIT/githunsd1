838ad650da941f9b9d8b9248382a29101debd217
SUGANYADEVI-GIT <kssuganyadevi@gmail.com>
2025-07-18 18:48:53 +0530

    Add files via upload

---
+++ maxNumber.js
@@ -0,0 +1,16 @@
+function findMaxNumber(numbers) {
+  let max = numbers[0];
+  for (let num of numbers) {
+    if (num > max) {
+      max = num;
+    }
+  }
+  return max;
+}
+
+// Example usage
+const numberArray = [23, 89, 12, 44, 95, 37];
+const maxValue = findMaxNumber(numberArray);
+
+// Using template literal
+console.log(`🔍 The maximum number in [${numberArray}] is ${maxValue}.`);
